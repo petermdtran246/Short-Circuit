04c9b90488cba713c89b2cbe9ce6cf20af1c85c6
petermdtran246 <petermdtran246@gmail.com>
2024-05-31 06:43:16 +0700

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Short Circuit
+    int a=10, b=5, i=5;
+    if(a>b && ++i<=b) {
+    }
+    cout << i << endl;
+
+    if(a<b || ++i<=b) {
+    }
+    cout << i <<endl;
+
+    return 0;
+}
